d8dffbfeec1e4f4c0442c77656ff38775e06da11
Syed Saud Ali <160472981+SyedSaudAli@users.noreply.github.com>
2024-02-19 14:29:31 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+let person_Name_With_Whitespace = "     \t \n       Syed Saud Ali           \t \n             ";
+console.log("Name with whitespace:", person_Name_With_Whitespace);
+let stripped_Name = person_Name_With_Whitespace.trim();
+console.log("Stripped name:", stripped_Name);
---
+++ app.ts
@@ -0,0 +1,4 @@
+let person_Name_With_Whitespace: string = "     \t \n       Syed Saud Ali           \t \n             ";
+console.log("Name with whitespace:", person_Name_With_Whitespace);
+let stripped_Name: string = person_Name_With_Whitespace.trim();
+console.log("Stripped name:", stripped_Name);
